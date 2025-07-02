6d57d9bdccf7be56d89dd22e229582e930d7510b
isac <isac@matchwornshirt.com>
2025-07-02 18:01:24 +0200

    Add project files.

---
+++ FundaMakelaarStats/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FundaMakelaarStats
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
